0aaf9e34f478527fb91a2ce8b9d9d4e5d14b677c
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-06 23:11:47 +0800

    works on firefox

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+var files = ['manifest.json', 'popup.html', 'icon-24x24.png', 'icon-48x48.png', 'icon-96x96.png', 'icon-128x128.png'];
+var xpiName = 'mxsh.zip';
+
+gulp.task('default', function () {
+  gulp.src(files)
+    .pipe(zip(xpiName))
+    .pipe(gulp.dest('.'));
+});
