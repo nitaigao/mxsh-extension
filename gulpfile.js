d09d5fb5392d89a94bbd80f2984e5417294a7fe8
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-18 12:30:27 +0800

    background page again

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 
-var files = ['manifest.json', 'popup.html', 'icon-24x24.png', 'icon-48x48.png', 'icon-96x96.png', 'icon-128x128.png'];
+var files = ['manifest.json', 'popup.html', 'background.html', 'icon-24x24.png', 'icon-48x48.png', 'icon-96x96.png', 'icon-128x128.png'];
 var xpiName = 'mxsh.zip';
 
 gulp.task('default', function () {
